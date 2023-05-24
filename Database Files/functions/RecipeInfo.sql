2109354392944cce0d2fa4bf77b6830a39d69aff
shanaglazer <shanaglazer1@gmail.com>
2023-05-24 09:35:27 +0300

    24

---
+++ Database Files/functions/RecipeInfo.sql
@@ -0,0 +1,13 @@
+create or alter function dbo.RecipeInfo(@RecipeId int)
+returns varchar(150)
+as
+begin
+
+	declare @return varchar(150) = ''
+
+
+
+	return @return
+
+end
+go
---
+++ Database Files/functions/TotalCaloriesForMeal.sql
@@ -0,0 +1,27 @@
+use HeartyHearthDB
+go
+
+create or alter function dbo.TotalCaloriesForMeal(@MealId int)
+returns int
+as 
+begin
+	
+	declare @return int = 0
+
+	SELECT @return = SUM(r.Calories)
+    from Recipe r
+    join RecipeCourse rc
+    on rc.RecipeID = r.RecipeID
+    join Course c
+    on c.CourseID = rc.CourseID
+    join meal m
+    on m.MealID = c.MealID
+    WHERE m.MealID = @MealId
+    group by r.Calories
+	
+	return @return
+
+end
+go
+
+select dbo.TotalCaloriesForMeal(m.MealID), * from Meal m
