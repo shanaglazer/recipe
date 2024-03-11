96cd233d029edd1b00fb59196c8830c639760ce7
shanaglazer <shanaglazer1@gmail.com>
2024-03-11 21:46:12 +0200

    15

---
+++ RecipeApps/RecipeAPI/MealController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using RecipeSystem;
+
+namespace RecipeAPI
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MealController : ControllerBase
+    {
+        //[HttpGet]
+        //public List<bizRecipe> Get()
+        //{
+        //    return new bizRecipe().GetList();
+        //}
+    }
+}
---
+++ RecipeApps/RecipeAPI/RecipeController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using RecipeSystem;
+
+namespace RecipeAPI
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RecipeController : ControllerBase
+    {
+        [HttpGet]
+        public List<bizRecipe> Get()
+        {
+            return new bizRecipe().GetList();
+        }
+    }
+}
