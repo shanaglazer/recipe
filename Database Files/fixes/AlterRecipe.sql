ca437e1178cf01a80a0cea3ecea675de96fd0c03
shanaglazer <shanaglazer1@gmail.com>
2024-06-17 21:03:59 +0300

    api c

--- Database Files/fixes/AlterRecipe.sql
+++ Database Files/fixes/AlterRecipe.sql
@@ -1,2 +1,8 @@
-alter table Recipe add Vegan bit not null default 0
+--alter table recipe drop constraint DF__Recipe__Vegan__44A0E595
+
+--alter table recipe drop column vegan
+
+alter table Recipe add Vegan bit  null 
 go
+
+select * from recipe
